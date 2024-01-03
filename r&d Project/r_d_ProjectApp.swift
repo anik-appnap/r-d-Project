6ca674c7d389b18cf4ea1398b09cd8e6ecbe070a
Asaduzzaman Anik <anik@appnap.io>
2024-01-03 15:55:27 +0600

    Initial Commit

---
+++ r&d Project/r_d_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  r_d_ProjectApp.swift
+//  r&d Project
+//
+//  Created by Appnap WS05 on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct r_d_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
